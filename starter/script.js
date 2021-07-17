7db841cc4ff2e9f3edab8dca90dd0779b642ab35
Jay For Short <jagoblacksky@gmail.com>
2021-07-17 02:27:59 -0500

    Lesson 59 Problem solving, Google , Stack Overflow

--- starter/script.js
+++ starter/script.js
@@ -6,3 +6,23 @@ const x = '23';
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(2040));
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+    // console.log(curTemp);
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
