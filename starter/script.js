9359912ea3fca6644f982ab3fd6a1cf8d5bf8e58
Jay For Short <jagoblacksky@gmail.com>
2021-07-18 02:12:31 -0500

    Lesson 59 Part 2: Problem solving

--- starter/script.js
+++ starter/script.js
@@ -6,6 +6,8 @@ const x = '23';
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(2040));
+// Problem 1:
+// We work for a company building a smart home thermometer.  "Given an array of temperatures of one day, calculate the temperature amplitude. There might sometimes be a sensor error."
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
@@ -26,3 +28,32 @@ const calcTempAmplitude = function (temps) {
 
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
+
+// Problem 2:
+// Function should now receive two arrays of temperatures
+// With two arrays, do we implement the same functionality twice? No.
+// Just merge two arrays
+
+// Break into sub problems
+// - How to merge two arrays?
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+    // console.log(curTemp);
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
